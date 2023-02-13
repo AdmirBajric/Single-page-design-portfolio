3095e5dfd9896e564f16028f16235fdab8013b30
Admir Bajric <admir.bajric.dev@gmail.com>
2023-02-13 22:27:11 +0100

    Update files

--- main.js
+++ main.js
@@ -3,14 +3,46 @@ const images = document.querySelectorAll(".slide");
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 
+let freezeListener = false;
+let x = "";
+let y = "";
 let current = 0;
 
-leftArrow.addEventListener("click", function () {
-  current--;
-  slider.style.transform = `translateX(${-current * 280}px)`;
-});
+const leftClick = () => {
+  y = "";
+  rightArrow.style.opacity = 1;
+  if (x === "") {
+    current--;
+    slider.style.transform = `translateX(${-current * 280}px)`;
+  }
+  if (
+    document
+      .querySelector(".projects-slider")
+      .style.transform.replace(/[^\d.]/g, "") === "560"
+  ) {
+    leftArrow.style.opacity = 0;
+    x = "560";
+  }
+};
 
-rightArrow.addEventListener("click", function () {
-  current++;
-  slider.style.transform = `translateX(${-current * 280}px)`;
-});
+leftArrow.addEventListener("click", leftClick);
+
+const rightClick = () => {
+  x = "";
+  leftArrow.style.opacity = 1;
+  if (y === "") {
+    current++;
+    slider.style.transform = `translateX(${-current * 280}px)`;
+  }
+
+  if (
+    document
+      .querySelector(".projects-slider")
+      .style.transform.replace(/[^\d.]/g, "") === "560"
+  ) {
+    rightArrow.style.opacity = 0;
+    y = "560";
+  }
+};
+
+rightArrow.addEventListener("click", rightClick);
