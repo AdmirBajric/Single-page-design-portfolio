91ebe5db16c6c3ed48db9ab75b9d4d1f3744702c
Admir Bajric <admir.bajric.dev@gmail.com>
2023-02-15 23:06:59 +0100

    Update files

--- main.js
+++ main.js
@@ -7,41 +7,43 @@ let x = "";
 let y = "";
 let current = 0;
 
-const leftClick = () => {
-  y = "";
-  rightArrow.style.opacity = 1;
-  if (x === "") {
-    current--;
-    slider.style.transform = `translateX(${-current * 280}px)`;
-  }
-  if (
-    document
-      .querySelector(".projects-slider")
-      .style.transform.replace(/[^\d.]/g, "") === "560"
-  ) {
-    leftArrow.style.opacity = 0;
-    x = "560";
-  }
-};
+if (window.innerWidth === "375px") {
+  const leftClick = () => {
+    y = "";
+    rightArrow.style.opacity = 1;
+    if (x === "") {
+      current--;
+      slider.style.transform = `translateX(${-current * 280}px)`;
+    }
+    if (
+      document
+        .querySelector(".projects-slider")
+        .style.transform.replace(/[^\d.]/g, "") === "560"
+    ) {
+      leftArrow.style.opacity = 0;
+      x = "560";
+    }
+  };
 
-leftArrow.addEventListener("click", leftClick);
+  leftArrow.addEventListener("click", leftClick);
 
-const rightClick = () => {
-  x = "";
-  leftArrow.style.opacity = 1;
-  if (y === "") {
-    current++;
-    slider.style.transform = `translateX(${-current * 280}px)`;
-  }
+  const rightClick = () => {
+    x = "";
+    leftArrow.style.opacity = 1;
+    if (y === "") {
+      current++;
+      slider.style.transform = `translateX(${-current * 280}px)`;
+    }
 
-  if (
-    document
-      .querySelector(".projects-slider")
-      .style.transform.replace(/[^\d.]/g, "") === "560"
-  ) {
-    rightArrow.style.opacity = 0;
-    y = "560";
-  }
-};
+    if (
+      document
+        .querySelector(".projects-slider")
+        .style.transform.replace(/[^\d.]/g, "") === "560"
+    ) {
+      rightArrow.style.opacity = 0;
+      y = "560";
+    }
+  };
 
-rightArrow.addEventListener("click", rightClick);
+  rightArrow.addEventListener("click", rightClick);
+}
