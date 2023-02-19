eee90103e9ea9e2f0fc65953720e7c902aa984bf
Admir Bajric <admir.bajric.dev@gmail.com>
2023-02-19 22:57:55 +0100

    Update files

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let x = "";
 let y = "";
 let current = 0;
 
-if (window.innerWidth === "375px") {
+if (window.innerWidth <= 375) {
   const leftClick = () => {
     y = "";
     rightArrow.style.opacity = 1;
@@ -39,6 +39,46 @@ if (window.innerWidth === "375px") {
       document
         .querySelector(".projects-slider")
         .style.transform.replace(/[^\d.]/g, "") === "560"
+    ) {
+      rightArrow.style.opacity = 0;
+      y = "560";
+    }
+  };
+
+  rightArrow.addEventListener("click", rightClick);
+} else if (window.innerWidth <= 768) {
+  console.log("radi");
+  const leftClick = () => {
+    y = "";
+    rightArrow.style.opacity = 1;
+    if (x === "") {
+      current--;
+      slider.style.transform = `translateX(${-current * 570}px)`;
+    }
+    if (
+      document
+        .querySelector(".projects-slider")
+        .style.transform.replace(/[^\d.]/g, "") === "1140"
+    ) {
+      leftArrow.style.opacity = 0;
+      x = "560";
+    }
+  };
+
+  leftArrow.addEventListener("click", leftClick);
+
+  const rightClick = () => {
+    x = "";
+    leftArrow.style.opacity = 1;
+    if (y === "") {
+      current++;
+      slider.style.transform = `translateX(${-current * 570}px)`;
+    }
+
+    if (
+      document
+        .querySelector(".projects-slider")
+        .style.transform.replace(/[^\d.]/g, "") === "1140"
     ) {
       rightArrow.style.opacity = 0;
       y = "560";
