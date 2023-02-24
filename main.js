a76ccadb6de1911cf3ada142e2334e7713a1f4f9
Admir Bajric <admir.bajric.dev@gmail.com>
2023-02-24 23:37:06 +0100

    Update files

--- main.js
+++ main.js
@@ -7,121 +7,58 @@ let x = "";
 let y = "";
 let current = 0;
 
-if (window.innerWidth <= 375) {
-  const leftClick = () => {
-    y = "";
-    rightArrow.style.opacity = 1;
-    if (x === "") {
-      current--;
-      slider.style.transform = `translateX(${-current * 280}px)`;
-    }
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "560"
-    ) {
-      leftArrow.style.opacity = 0;
-      x = "560";
-    }
-  };
+let imgDimension = 570;
 
-  leftArrow.addEventListener("click", leftClick);
-
-  const rightClick = () => {
-    x = "";
-    leftArrow.style.opacity = 1;
-    if (y === "") {
-      current++;
-      slider.style.transform = `translateX(${-current * 280}px)`;
-    }
-
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "560"
-    ) {
-      rightArrow.style.opacity = 0;
-      y = "560";
-    }
-  };
-
-  rightArrow.addEventListener("click", rightClick);
-} else if (window.innerWidth <= 768) {
-  const leftClick = () => {
-    y = "";
-    rightArrow.style.opacity = 1;
-    if (x === "") {
-      current--;
-      slider.style.transform = `translateX(${-current * 570}px)`;
-    }
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "1140"
-    ) {
-      leftArrow.style.opacity = 0;
-      x = "560";
-    }
-  };
-
-  leftArrow.addEventListener("click", leftClick);
-
-  const rightClick = () => {
-    x = "";
-    leftArrow.style.opacity = 1;
-    if (y === "") {
-      current++;
-      slider.style.transform = `translateX(${-current * 570}px)`;
-    }
-
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "1140"
-    ) {
-      rightArrow.style.opacity = 0;
-      y = "560";
-    }
-  };
-
-  rightArrow.addEventListener("click", rightClick);
-} else {
-  const leftClick = () => {
-    y = "";
-    rightArrow.style.opacity = 1;
-    if (x === "") {
-      current--;
-      slider.style.transform = `translateX(${-current * 570}px)`;
-    }
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "1140"
-    ) {
-      leftArrow.style.opacity = 0;
-      x = "560";
-    }
-  };
-
-  leftArrow.addEventListener("click", leftClick);
-
-  const rightClick = () => {
-    x = "";
-    leftArrow.style.opacity = 1;
-    if (y === "") {
-      current++;
-      slider.style.transform = `translateX(${-current * 570}px)`;
-    }
-
-    if (
-      document
-        .querySelector(".projects-slider")
-        .style.transform.replace(/[^\d.]/g, "") === "1140"
-    ) {
-      rightArrow.style.opacity = 0;
-      y = "560";
-    }
-  };
-
-  rightArrow.addEventListener("click", rightClick);
+function reportWindowSize() {
+  if (window.innerWidth <= 375) {
+    imgDimension = 280;
+  } else if (window.innerWidth <= 768) {
+    imgDimension = 570;
+  }
 }
+reportWindowSize();
+window.onresize = reportWindowSize;
+
+function sum() {
+  let sumDim = imgDimension * 2;
+  return sumDim.toString();
+}
+
+const leftClick = () => {
+  y = "";
+  rightArrow.style.opacity = 1;
+  if (x === "") {
+    current--;
+    slider.style.transform = `translateX(${-current * imgDimension}px)`;
+  }
+  if (
+    document
+      .querySelector(".projects-slider")
+      .style.transform.replace(/[^\d.]/g, "") === sum()
+  ) {
+    leftArrow.style.opacity = 0;
+    x = sum();
+  }
+};
+
+leftArrow.addEventListener("click", leftClick);
+
+const rightClick = () => {
+  x = "";
+  leftArrow.style.opacity = 1;
+  if (y === "") {
+    current++;
+    slider.style.transform = `translateX(${-current * imgDimension}px)`;
+  }
+
+  if (
+    document
+      .querySelector(".projects-slider")
+      .style.transform.replace(/[^\d.]/g, "") === sum()
+  ) {
+    rightArrow.style.opacity = 0;
+    y = sum();
+  }
+};
+
+rightArrow.addEventListener("click", rightClick);
